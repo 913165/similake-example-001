678de87345f963e15cfdbe8402c9e6175af96ece
Pritesh <tinumistry@gmail.com>
2024-12-28 03:18:52 +0530

    first commit

---
+++ src/main/java/com/example/chatcli/similakeexample001/controller/AppController.java
@@ -0,0 +1,35 @@
+package com.example.chatcli.similakeexample001.controller;
+
+
+
+import com.example.chatcli.similakeexample001.service.AIService;
+import org.springframework.ai.document.Document;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+public class AppController {
+
+    @Autowired
+    AIService appService;
+
+    @GetMapping("/loaddata")
+    public String loaddata() {
+        appService.loaddata();
+        return "Data loaded successfully";
+    }
+
+    @GetMapping("/search")
+    public List<String> search(@RequestParam String query) {
+        List<Document> docs = appService.search(query);
+        List<String> docContents = docs.stream()
+                .map(Document::getContent)
+                .collect(Collectors.toList());
+        return docContents;
+    }
+}
---
+++ src/test/java/com/example/chatcli/similakeexample001/SimilakeExample001ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatcli.similakeexample001;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimilakeExample001ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
